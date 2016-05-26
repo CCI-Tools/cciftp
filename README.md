e8d0cafbc6480590890085a54c8fd4a77e98f5be
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-05-26 17:27:13 +0200

    added new options --start_dir and --max_depth

--- README.md
+++ README.md
@@ -4,14 +4,19 @@ Tool(s) that help staying up-to-date with ESA's CCI Data Portal's (http://cci.es
 
 Usage:
 
-    usage: scan.py [-h] [-t] [DIR]
-
+    usage: cci-ftp-scan [-h] [-s START] [-d NUM] [-t] [TARGET]
+    
     Scan the ESA CCI Portal's FPT tree.
-
+    
     positional arguments:
-      DIR             Target directory to which the scan results will be written.
-                      Default is current directory.
-
+      TARGET                Target directory to which the scan results will be
+                            written. Default is current directory.
+    
     optional arguments:
-      -h, --help      show this help message and exit
-      -t, --toc_only  print out terms and conditions and exit
+      -h, --help            show this help message and exit
+      -s START, --start_dir START
+                            directory within the FTP directory from which to start
+                            scanning
+      -d NUM, --max_depth NUM
+                            maximum directory scanning depth
+      -t, --toc_only        print out terms and conditions and exit
--- cciftp/scan.py
+++ cciftp/scan.py
@@ -22,24 +22,32 @@ class EsaCciFtpDataSource:
         ftp.quit()
         return '\n'.join(lines)
 
-    def scan_to(self, target_dir):
+    def scan_to(self, target_dir, start_dir, max_depth):
         ftp = FTP(self.host)
         ftp.login()
 
-        target_path = os.path.join(target_dir, 'esa_cci_ftp_tree')
+        target_path = '%s/%s' % (target_dir, 'esa_cci_ftp_tree')
+        if start_dir:
+            target_path = '%s/%s' % (target_path, start_dir)
+
         if not os.path.exists(target_path):
-            os.mkdir(target_path)
+            os.makedirs(target_path)
 
         with open(os.path.join(target_dir, 'esa_cci_ftp_index.txt'), 'w') as index_file:
-            self._rec_dir_scan(ftp, index_file, target_path, self.dir_name, 0)
+            self._rec_dir_scan(ftp,
+                               index_file,
+                               target_path,
+                               '%s/%s' % (self.dir_name, start_dir) if start_dir else self.dir_name,
+                               0, max_depth)
 
         ftp.quit()
 
-    def _rec_dir_scan(self, ftp, index_file, target_path, dir_name, level):
+    def _rec_dir_scan(self, ftp, index_file, target_path, dir_name, level, max_level):
         ftp.cwd(dir_name)
         content = list(ftp.mlsd(facts=['type', 'size', 'modify']))
         indent = (level * '  ')
         file_size_sum = 0
+        current_size = 0
         for name, facts in content:
             t = facts.get('type', None)
             if t == 'dir':
@@ -49,7 +57,8 @@ class EsaCciFtpDataSource:
                 target_dir = os.path.join(target_path, name)
                 if not os.path.exists(target_dir):
                     os.mkdir(target_dir)
-                current_size = self._rec_dir_scan(ftp, index_file, target_dir, name, level + 1)
+                if max_level is None or level < max_level:
+                    current_size = self._rec_dir_scan(ftp, index_file, target_dir, name, level + 1, max_level)
             elif t == 'file':
                 line = indent + name
                 print(line)
@@ -70,9 +79,13 @@ class EsaCciFtpDataSource:
 def main(args=sys.argv[1:]):
     import argparse
     parser = argparse.ArgumentParser(description="Scan the ESA CCI Portal's FPT tree.")
-    parser.add_argument('target_dir', metavar='DIR', type=str, default='.', nargs='?',
+    parser.add_argument('target_dir', metavar='TARGET', type=str, default='.', nargs='?',
                         help='Target directory to which the scan results will be written. '
                              'Default is current directory.')
+    parser.add_argument('-s', '--start_dir', metavar='START', type=str, nargs=1, action='store',
+                        help='directory within the FTP directory from which to start scanning')
+    parser.add_argument('-d', '--max_depth', metavar='NUM', type=int, nargs=1, action='store',
+                        help='maximum directory scanning depth')
     parser.add_argument('-t', '--toc_only', action='store_true',
                         help='print out terms and conditions and exit')
 
@@ -86,11 +99,14 @@ def main(args=sys.argv[1:]):
         print(tac)
         print()
     else:
-        if not os.path.exists(args.target_dir):
-            print('error: invalid target directory:', args.target_dir)
+        target_dir = args.target_dir
+        if not os.path.exists(target_dir):
+            print('error: invalid target directory:', target_dir)
             exit(1)
-        print('Scanning to', args.target_dir)
-        data_source.scan_to(args.target_dir)
+        start_dir = args.start_dir[0] if args.start_dir is not None else ''
+        max_depth = args.max_depth[0] if args.max_depth is not None else None
+        print('Scanning %s/** to %s' % (start_dir, args.target_dir))
+        data_source.scan_to(target_dir, start_dir, max_depth)
 
 
 if __name__ == '__main__':
